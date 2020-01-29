6e615ba818c4ac9d926bd33b2f37135434b4216d
michelspirlandeli <michelspirlandeli10@gmail.com>
2020-01-28 22:47:56 -0300

    add project

---
+++ src/main/java/br/com/deveficiente/apiCasaCodigo/ApiCasaCodigoApplication.java
@@ -0,0 +1,13 @@
+package br.com.deveficiente.apiCasaCodigo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiCasaCodigoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiCasaCodigoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/deveficiente/apiCasaCodigo/ApiCasaCodigoApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.deveficiente.apiCasaCodigo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiCasaCodigoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
